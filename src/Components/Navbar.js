5788b51825764cc7f1e073a5505892a0a1e72039
Uday Mehtani <udaymehtani@gmail.com>
2023-04-16 06:18:39 +0530

    Added open in new tab option

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,13 +3,13 @@ import "./Navbar.css";
 const Navbar = () => {
   return (
     <div className="Navbar">
-      <a href="https://traya.health/" className="logo">
+      <a href="https://traya.health/" className="logo" target="_blank" rel="noreferrer" >
         <img
           src="https://form.traya.health/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Ftraya.a5a9cff0.png&w=128&q=75"
           alt="Traya-Logo"
         />
       </a>
-      <a href="#">
+      <a href="https://traya.health/" target="_blank" rel="noreferrer" >
         <span className="exit">Exit</span>
       </a>
     </div>
